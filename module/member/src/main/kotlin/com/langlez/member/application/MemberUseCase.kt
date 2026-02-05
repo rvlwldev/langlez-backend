83501827e49a460fb2c25ec072da8532877cb028
rvlwldev <rvlwldev7@gmail.com>
2026-02-05 18:59:15 +0900

    refactor: Member 모듈 리팩토링 및 DTO 검증 추가

--- module/member/src/main/kotlin/com/langlez/member/application/MemberUseCase.kt
+++ module/member/src/main/kotlin/com/langlez/member/application/MemberUseCase.kt
@@ -1,5 +1,6 @@
 package com.langlez.member.application
 
+import com.langlez.member.domain.LanguageLevel
 import com.langlez.member.domain.Member
 
 data class CreateMemberCommand(
@@ -10,11 +11,27 @@ data class CreateMemberCommand(
     val providerId: String,
 )
 
+data class TargetLanguageCommandDto(
+    val language: String,
+    val level: LanguageLevel,
+)
+
 data class UpdateMemberCommand(
     val nickname: String,
     val profileImageUrl: String?,
+    val additionalProfileImages: List<String>?,
+    val locationCountry: String?,
+    val locationCity: String?,
+    val nationality: String?,
+    val interests: List<String>?,
+    val mbti: String?,
+    val nativeLanguage: String?,
+    val targetLanguages: List<TargetLanguageCommandDto>?,
+    val wishDestinations: List<String>?,
+    val visitedDestinations: List<String>?,
 )
 
 interface MemberUseCase {
     fun findOrCreateMember(command: CreateMemberCommand): Member
+    fun getMember(email: String): Member
 }
