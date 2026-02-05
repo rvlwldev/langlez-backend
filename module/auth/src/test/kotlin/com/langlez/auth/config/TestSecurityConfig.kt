a5cfba14e535f3b7692fea5fda065ac8addb434f
rvlwldev <rvlwldev7@gmail.com>
2026-02-05 18:59:32 +0900

    test: Auth 및 Security 테스트 추가 및 개선

---
+++ module/auth/src/test/kotlin/com/langlez/auth/config/TestSecurityConfig.kt
@@ -0,0 +1,19 @@
+package com.langlez.auth.config
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@TestConfiguration
+@EnableWebSecurity
+class TestSecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { it.anyRequest().permitAll() }
+        return http.build()
+    }
+}
