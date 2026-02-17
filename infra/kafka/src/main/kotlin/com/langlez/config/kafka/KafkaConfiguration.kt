8312f167752f6ebdd2c4550ab489b45c65a00ac1
rvlwldev <rvlwldev7@gmail.com>
2026-02-18 06:10:15 +0900

    chore: 도커 설정 구조 정리 및 .gitignore 업데이트

---
+++ infra/kafka/src/main/kotlin/com/langlez/config/kafka/KafkaConfiguration.kt
@@ -0,0 +1,58 @@
+package com.langlez.config.kafka
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+@Configuration
+@EnableConfigurationProperties(KafkaProperties::class)
+class KafkaConfiguration(private val kafkaProperties: KafkaProperties) {
+    @Bean
+    fun producerFactory(): ProducerFactory<String, Any> {
+        val configProps = HashMap<String, Any>()
+
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaProperties.bootstrapServers
+        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
+
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, Any> = KafkaTemplate(producerFactory())
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, Any> {
+        val configProps = HashMap<String, Any>()
+
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaProperties.bootstrapServers
+        configProps[ConsumerConfig.GROUP_ID_CONFIG] = kafkaProperties.consumer.groupId
+        configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+        configProps[JsonDeserializer.TRUSTED_PACKAGES] = "*"
+
+        return DefaultKafkaConsumerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+
+        factory.consumerFactory = consumerFactory()
+
+        return factory
+    }
+}
