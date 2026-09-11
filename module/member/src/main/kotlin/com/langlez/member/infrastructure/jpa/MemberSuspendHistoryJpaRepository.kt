051759ce0876405f1c020eda4076de5d34f59bfa
rvlwldev <rvlwldev7@gmail.com>
2026-09-11 10:28:21 +0900

    refactor(member): MemberSuspendHistory.releaseActive 를 JPQL 에서 QueryDSL 로 전환

--- module/member/src/main/kotlin/com/langlez/member/infrastructure/jpa/MemberSuspendHistoryJpaRepository.kt
+++ module/member/src/main/kotlin/com/langlez/member/infrastructure/jpa/MemberSuspendHistoryJpaRepository.kt
@@ -2,13 +2,5 @@ package com.langlez.member.infrastructure.jpa
 
 import com.langlez.member.domain.MemberSuspendHistory
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
-interface MemberSuspendHistoryJpaRepository : JpaRepository<MemberSuspendHistory, Long> {
-
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("update MemberSuspendHistory h set h.isReleased = true where h.memberId = :memberId and h.isReleased = false")
-    fun releaseActive(@Param("memberId") memberId: Long)
-}
+interface MemberSuspendHistoryJpaRepository : JpaRepository<MemberSuspendHistory, Long>
